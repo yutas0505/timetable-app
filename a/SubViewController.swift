39ad674095af12afbe03ef36a8fb4f343de923fa
yutas0505 <yutas0505@icloud.com>
2017-04-30 17:47:55 +0900

    first commit

---
+++ a/SubViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SubViewController.swift
+//  a
+//
+//  Created by 佐比内優太 on 2017/04/30.
+//  Copyright © 2017年 Yuta Sahinai. All rights reserved.
+//
+
+import UIKit
+
+class SubViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
